207d928075f2813602cbe342136f03ffc5a1393d
Christina Loiacono <loiaconochristina@gmail.com>
2022-01-18 16:10:10 -0500

    created transactions data

---
+++ models/transactions.js
@@ -0,0 +1,51 @@
+module.exports = [
+    {
+      date: "April 1",
+      name: "Groceries",
+      amount: -30,
+      from: "Ideal Supermarket",
+      category: "food",
+    },
+    {
+      date: "April 1",
+      name: "Shopping",
+      amount: -47,
+      from: "Target",
+      category: "shopping",
+    },
+    {
+      date: "April 2",
+      name: "Income",
+      amount: 2000,
+      from: "Google",
+      category: "finance",
+    },
+    {
+      date: "April 5",
+      name: "Gas",
+      amount: -25,
+      from: "Shell Gas Station",
+      category: "transportation",
+    },
+    {
+      date: "April 6",
+      name: "Rent",
+      amount: -700,
+      from: "Pike International, LLC",
+      category: "home"
+    },
+    {
+      date: "April 8",
+      name: "Income",
+      amount: 2000,
+      from: "Google",
+      category: "finance",
+    },
+    {
+      date: "April 8",
+      name: "Groceries",
+      amount: -40,
+      from: "Mr. Kiwi Fruit Store",
+      category: "food",
+    },
+  ];
